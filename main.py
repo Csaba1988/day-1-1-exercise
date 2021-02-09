6e917cf13808f658f76b9bdd569814eacaf3e8e9
CsabaVass <vasscsaba880127@gmail.com>
2021-02-09 21:26:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#Write your code below this line 👇
+print("Day 1 - Python Print Function");
+print("The function is declared like this:");
+print("print ('what to print')");
+
+#use one print function
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint ('what to print')");
+
+
+
+
+
+
+
+
+
